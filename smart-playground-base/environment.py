6002a32d1bcf7066eef6ab46178fd28e7885ca1a
AgostinoRizzo <rzzgtn96p08d086j@studenti.unical.it>
2020-10-21 23:34:03 +0200

    Environment sounds playing added.
    Sounds are played using the Pygame library.

--- smart-playground-base/environment.py
+++ smart-playground-base/environment.py
@@ -1,17 +1,30 @@
 """" sounds and light environment management """
 
-#import pygame
-#import resources
+from pygame import init
+from pygame.mixer import pre_init, Sound
+import resources
+import time
 
-#pygame.mixer.pre_init(buffer=1024)
-#pygame.init()
+pre_init(buffer=1024)
+init()
 
-racket_hit_sound = None #pygame.mixer.Sound(res.RACKET_HIT_SOUND_FILENAME)
-ball_hit_sound = None #pygame.mixer.Sound(res.BALL_HIT_SOUND_FILENAME)
+racket_swing_sound = Sound(resources.RACKET_SWING_SOUND_FILENAME)
+racket_hit_sound = Sound(resources.RACKET_HIT_SOUND_FILENAME)
+ball_hit_sound = Sound(resources.BALL_HIT_SOUND_FILENAME)
 
-
+def play_racket_swing_sound():
+    racket_swing_sound.play()
+    
 def play_racket_hit_sound():
-    pass
+    racket_hit_sound.play()
 
 def play_ball_hit_sound():
-    pass
+    ball_hit_sound.play()
+
+if __name__ == '__main__':
+    play_racket_swing_sound()
+    time.sleep(3)
+    play_racket_hit_sound()
+    time.sleep(5)
+    play_ball_hit_sound()
+    time.sleep(3)
--- smart-playground-base/resources.py
+++ smart-playground-base/resources.py
@@ -4,6 +4,6 @@ import os
 
 PROGRAM_FOLDER = os.path.abspath(os.path.dirname(__file__))
 
-RACKET_SWING_SOUND_FILENAME = os.path.join(PROGRAM_FOLDER, 'res/raw/RacketSwing.wav')
-RACKET_HIT_SOUND_FILENAME = os.path.join(PROGRAM_FOLDER, 'res/raw/RacketHit.wav')
-BALL_HIT_SOUND_FILENAME = os.path.join(PROGRAM_FOLDER, 'res/raw/BallHit.wav')
+RACKET_SWING_SOUND_FILENAME = os.path.join(PROGRAM_FOLDER, 'resources/raw/racket_swing.wav')
+RACKET_HIT_SOUND_FILENAME = os.path.join(PROGRAM_FOLDER, 'resources/raw/racket_hit.wav')
+BALL_HIT_SOUND_FILENAME = os.path.join(PROGRAM_FOLDER, 'resources/raw/ball_hit.wav')
