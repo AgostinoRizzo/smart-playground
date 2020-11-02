2fb2693a8663c6a7a3e283f83ce5caf1d0b1cd37
AgostinoRizzo <rzzgtn96p08d086j@studenti.unical.it>
2020-11-02 16:21:18 +0100

    Full ball status updating.

--- smart-playground-base/playground.py
+++ smart-playground-base/playground.py
@@ -20,20 +20,36 @@ class PlaygroundBaseStatus:
     
     def __init__(self):
         self.ballLocation = PointLocation(-1.0, -1.0)
+        self.ballOrientation = None
         self.lock = RLock()
     
     def update_ball_location(self, left, top):
         with self.lock:
             self.ballLocation.left = left
             self.ballLocation.top = top
+            print("New ball location: %f, %f" % (left, top))
+    
+    def update_ball_orientation(self, orientation):
+        with self.lock:
+            self.ballOrientation = orientation
+            print("New ball orientation: %d" % orientation)
+    
+    def set_unknown_ball_status(self):
+        with self.lock:
+            self.ballLocation.left = -1.0
+            self.ballLocation.top = -1.0
+            self.ballOrientation = -1
+            print("Ball status unknown.")
     
     def get_ball_location(self) -> PointLocation:
         with self.lock:
-            return PointLocation(self.ballLocation.left, self.ballLocation.top)
+            if self.__check_ball_status():
+                return PointLocation(self.ballLocation.left, self.ballLocation.top)
+            return None
     
     def is_ball_in(self) -> bool:
         with self.lock:
-            return self.ballLocation.left < 0.0 or self.ballLocation.top < 0.0
+            return self.__check_ball_status()
     
     def get_ascii_status(self):
         full_width = 50
@@ -48,17 +64,22 @@ class PlaygroundBaseStatus:
             status[i][0] = status[i][-1] = '#'
         
         with self.lock:
-            status[int((height - 1) * self.ballLocation.top) + 1][int((width - 1) * self.ballLocation.left) + 1] = '@'
+            if self.__check_ball_status():
+                status[int((height - 1) * self.ballLocation.top) + 1][int((width - 1) * self.ballLocation.left) + 1] = '@'
             ascii_status = ''
             for i in range(full_height):
                 ascii_status += ' '.join(status[i]) + "\n"
             return ascii_status
+    
+    def __check_ball_status(self) -> bool:
+        return self.ballLocation.left >= 0.0 and self.ballLocation.top >= 0.0 and self.ballOrientation >= 0.0
 
 
 class BallTracker(Thread):
     
     BALL_DATA_SOCKET_READ_TIMEOUT = 2.0
-    BALL_DATA_BUFFER_SIZE = 12
+    MAX_BALL_DATA_BUFFER_SIZE = 14
+    BALL_STATUS_DATA_BUFFER_SIZES = [4, 6, 12, 14]
     
     def __init__(self, playgroundBaseStatus:PlaygroundBaseStatus):
         Thread.__init__(self)
@@ -81,7 +102,7 @@ class BallTracker(Thread):
         
         while not self.stopEvent.is_set():
             try:
-                databuff, _ = self.ballDataSocket.recvfrom(BallTracker.BALL_DATA_BUFFER_SIZE)
+                databuff, _ = self.ballDataSocket.recvfrom(BallTracker.MAX_BALL_DATA_BUFFER_SIZE)
                 self.__on_ball_data(databuff)
                     
             except socket.timeout:
@@ -89,17 +110,36 @@ class BallTracker(Thread):
     
     def __on_ball_data(self, databuff):
         
-        if len(databuff) != BallTracker.BALL_DATA_BUFFER_SIZE:
+        databuff_length = len(databuff)
+        
+        if databuff_length not in BallTracker.BALL_STATUS_DATA_BUFFER_SIZES:
             return
         
-        seqnumber, = struct.unpack_from('>i', databuff, 0)
-        ball_left, = struct.unpack_from('>f', databuff, 4)
-        ball_top, = struct.unpack_from('>f', databuff, 8)
+        buff_offset = 0
         
-        if seqnumber > self.seqdata_number:
+        seqnumber, = struct.unpack_from('>i', databuff, buff_offset)
+        buff_offset += 4
+        
+        if seqnumber <= self.seqdata_number:
+            return
+        
+        if databuff_length >= 12:
+            
+            ball_left, = struct.unpack_from('>f', databuff, buff_offset)
+            buff_offset += 4
+            ball_top, = struct.unpack_from('>f', databuff, buff_offset)
+            buff_offset += 4
             self.playgroundBaseStatus.update_ball_location(ball_left, ball_top)
-            print(self.playgroundBaseStatus.get_ascii_status())
-            self.seqdata_number += 1
+            
+        if databuff_length == 6 or databuff_length == 14:
+            ball_orientation, = struct.unpack_from('>h', databuff, buff_offset)
+            buff_offset += 2
+            self.playgroundBaseStatus.update_ball_orientation(ball_orientation)
+        
+        if databuff_length == 4:
+            self.playgroundBaseStatus.set_unknown_ball_status()
+        
+        self.seqdata_number += 1
     
 
 
